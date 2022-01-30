c156adb5e84dfcff7f647638e75be78a66a6b495
Magali <magali.thuaire@gmail.com>
2022-01-30 16:25:38 +0100

    🐞 Fixed JS Bootstrap fixtures

---
+++ app/views/base.php
@@ -0,0 +1,16 @@
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Blog Magali</title>
+        <link href="../public/build/app.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="../public/build/app.js" defer></script>
+    </head>
+    <body>
+        <main class="container mt-4">
+            <h1>Mon blog</h1>
+        </main>
+    </body>
+</html>
