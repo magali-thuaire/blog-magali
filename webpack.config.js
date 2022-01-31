f49b7d5698e6f22d766b22c3562deaab3f0fdc8a
Magali <magali.thuaire@gmail.com>
2022-01-31 11:08:56 +0100

    🔧 Added JS Webpack Plugins Copy and Clean

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -11,9 +13,17 @@ const config = {
     path: path.resolve(__dirname, "public/build"),
     filename: 'app.js'
   },
-  plugins: [new MiniCssExtractPlugin({
-    filename: 'app.css'
-  })],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app.css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./assets/images", to: "images" },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
   module: {
     rules: [
       {
@@ -32,7 +42,16 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
+      // {
+      //   test: /\.(jpe?g|png|gif|svg|ico)$/i,
+      //   use: [{
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[ext]',
+      //       outputPath: 'images/'
+      //     }
+      //   }]
+      // }
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
