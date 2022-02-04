7660c9ca3540bd0e5c5d5b11b73d0c37e80ef0aa
Magali <magali.thuaire@gmail.com>
2022-02-04 11:17:07 +0100

    ✨ Added Date in ContactEntity

--- app/Manager/ContactManager.php
+++ app/Manager/ContactManager.php
@@ -4,18 +4,23 @@ namespace App\Manager;
 
 use App\Entity\ContactEntity;
 use Core\Manager\EntityManager;
+use DateTime;
 
 class ContactManager extends EntityManager
 {
 
 	public function new(ContactEntity $contact)
 	{
+
+		$contact->setDate(new DateTime());
+
 		return $this->execute(
-			"INSERT INTO contact(name, email, message) VALUES (:name, :email, :message);",
+			"INSERT INTO contact(name, email, message, date) VALUES (:name, :email, :message, :date);",
 			[
 				'name'    => $contact->getName(),
 				'email'   => $contact->getEmail(),
 				'message' => $contact->getMessage(),
+				'date'	  => $contact->getDate()->format('Y-m-d H:i:s')
 			]
 		);
 	}
