a2415d1493a52c712ecbab3ffa6e64fa726f69f2
Magali <magali.thuaire@gmail.com>
2022-02-11 16:18:52 +0100

    🌀 [CONFIG] Refactored App Models

--- app/Model/ContactMail.php
+++ app/Model/ContactMail.php
@@ -7,17 +7,15 @@ use Core\Model\Mail;
 
 class ContactMail extends Mail
 {
+    public function sendEmail(ContactEntity $contact, $to = EMAIL_DEFAULT_TO, $subject = EMAIL_DEFAULT_SUBJECT): bool
+    {
+        $this
+            ->setTo($to)
+            ->setSubject($subject)
+            ->setMessage($contact->getMessage())
+            ->setHeader('From: ' . $contact->getName() . '<' . $contact->getEmail() . '>')
+        ;
 
-	public function sendEmail(ContactEntity $contact, $to = EMAIL_DEFAULT_TO, $subject = EMAIL_DEFAULT_SUBJECT): bool
-	{
-		$this
-			->setTo($to)
-			->setSubject($subject)
-			->setMessage($contact->getMessage())
-			->setHeader('From: ' . $contact->getName() . '<' . $contact->getEmail() . '>')
-		;
-
-		return mail($this->to, $this->subject, $this->message, $this->header);
-	}
-
+        return mail($this->to, $this->subject, $this->message, $this->header);
+    }
 }
