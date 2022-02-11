16202ddd5f9035817805f556f3d3ebde1d2785d0
Magali <magali.thuaire@gmail.com>
2022-02-11 16:56:54 +0100

    🌀 [CONFIG] Refactored Core Models

--- core/Model/DateTrait.php
+++ core/Model/DateTrait.php
@@ -6,30 +6,31 @@ use DateTime;
 
 trait DateTrait
 {
-	private static $_months = [
-		'january' 	=> 'janvier',
-		'february' 	=> 'février',
-		'march' 	=> 'mars',
-		'april' 	=> 'avril',
-		'may' 		=> 'mai',
-		'june' 		=> 'juin',
-		'july' 		=> 'juillet',
-		'august' 	=> 'août',
-		'september' => 'septembre',
-		'october' 	=> 'octobre',
-		'november' 	=> 'novembre',
-		'december' 	=> 'décembre',
-	];
+    private static array $months = [
+        'january'   => 'janvier',
+        'february'  => 'février',
+        'march'     => 'mars',
+        'april'     => 'avril',
+        'may'       => 'mai',
+        'june'      => 'juin',
+        'july'      => 'juillet',
+        'august'    => 'août',
+        'september' => 'septembre',
+        'october'   => 'octobre',
+        'november'  => 'novembre',
+        'december'  => 'décembre',
+    ];
 
-	private function dateFormatted(DateTime $dateTime, $format = 'Y-d-m h:i:s') {
+    private function dateFormatted(DateTime $dateTime, $format = 'Y-d-m h:i:s'): array|string
+    {
 
-		$date = $dateTime->format($format);
-		foreach (self::$_months as $month => $mois) {
-			if(strpos(strtolower($date), $month)) {
-				$date = str_replace($month, $mois, strtolower($date));
-			}
-		}
+        $date = $dateTime->format($format);
+        foreach (self::$months as $month => $mois) {
+            if (strpos(strtolower($date), $month)) {
+                $date = str_replace($month, $mois, strtolower($date));
+            }
+        }
 
-		return $date;
-	}
+        return $date;
+    }
 }
