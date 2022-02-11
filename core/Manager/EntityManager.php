b4c40944310e73c75c28307d62c20ac140feb847
Magali <magali.thuaire@gmail.com>
2022-02-11 16:53:05 +0100

    🌀 [CONFIG] Refactored Core Managers

--- core/Manager/EntityManager.php
+++ core/Manager/EntityManager.php
@@ -6,45 +6,43 @@ use Core\Database\Database;
 
 class EntityManager
 {
+    protected string|array|false $entity;
+    protected Database $db;
 
-	protected $entity;
-	protected $db;
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
 
-	public function __construct(Database $db)
-	{
-		$this->db = $db;
+        $part = explode('\\', get_class($this));
+        $class_name = end($part);
+        $this->entity = str_replace('Manager', 'Entity', $class_name);
+    }
 
-		if(is_null($this->entity)) {
-			$part = explode('\\', get_class($this));
-			$class_name = end($part);
-			$this->entity = str_replace('Manager', 'Entity', $class_name);
-		}
-	}
+    public function execute($statement, $attributs, $insert = false): int|string
+    {
+        return $this->db->execute($statement, $attributs, $insert);
+    }
 
-	public function execute($statement, $attributs, $insert = false) {
-		return $this->db->execute($statement, $attributs, $insert);
-	}
+    public function query($statement, $fetchClass = false): bool|array
+    {
 
-	public function query($statement, $fetchClass = false) {
+        if ($fetchClass) {
+            $fetchClass =  str_replace('Manager', 'Entity', get_class($this));
+        } else {
+            $fetchClass = '';
+        }
 
-		if($fetchClass) {
-			$fetchClass =  str_replace('Manager', 'Entity', get_class($this));
-		} else {
-			$fetchClass = '';
-		}
+        return $this->db->query($statement, $fetchClass);
+    }
 
-		return $this->db->query($statement, $fetchClass);
-	}
+    public function prepare($statement, $attributs, $one = false, $fetchClass = false)
+    {
 
-	public function prepare($statement, $attributs, $one = false, $fetchClass = false) {
-
-		if($fetchClass) {
-			$fetchClass =  str_replace('Manager', 'Entity', get_class($this));
-		} else {
-			$fetchClass = '';
-		}
-		return $this->db->prepare($statement, $attributs, $one, $fetchClass);
-
-	}
-
+        if ($fetchClass) {
+            $fetchClass =  str_replace('Manager', 'Entity', get_class($this));
+        } else {
+            $fetchClass = '';
+        }
+        return $this->db->prepare($statement, $attributs, $one, $fetchClass);
+    }
 }
